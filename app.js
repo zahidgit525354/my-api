80eae42674f41ab711e59a22c11bd9fd3b09f8f2
Zahid Win <zahidWin@example.com>
2023-09-17 14:28:31 -0700

    Add all routes

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var app = express();
 
-app.get('/home', function(req, res) {
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
     console.log("Got a GET request for the homepage");
-    res.send("Hello GET");
-
-})
-
-app.post('/create', function(req, res){
-    console,log("Got a POST Request");
-    res.send("Create Content");
-})
+    res.send('Hello GET');
+ })
+ 
+ // This responds a POST request for the homepage
+ app.post('/', function (req, res) {
+    console.log("Got a POST request for the homepage");
+    res.send('Hello POST');
+ })
+ 
+ // This responds a DELETE request for the /del_user page.
+ app.delete('/del_user', function (req, res) {
+    console.log("Got a DELETE request for /del_user");
+    res.send('Hello DELETE');
+ })
+ 
+ // This responds a GET request for the /list_user page.
+ app.get('/list_user', function (req, res) {
+    console.log("Got a GET request for /list_user");
+    res.send('Page Listing');
+ })
+ 
+ // This responds a GET request for abcd, abxcd, ab123cd, and so on
+ app.get('/ab*cd', function(req, res) {   
+    console.log("Got a GET request for /ab*cd");
+    res.send('Page Pattern Match');
+ })
 // Create Server
 
 var Server = app.listen(3000, function(){
